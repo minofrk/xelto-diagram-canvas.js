8eea69cccd55174cdb02d2131b5734ab9ce9185b
古城みの <minofrk@gmail.com>
2019-06-20 22:58:07 +0900

    整理

--- src/render-diagram/background-image/make-label-image.ts
+++ src/render-diagram/background-image/make-label-image.ts
@@ -10,33 +10,36 @@ const commonProps = {
     virtualFontSize: 0.9,
 };
 
+function makeAlsiaLabelBrush(i: number, leftTop: Point): Brush {
+    return fillText({
+        ...commonProps,
+        value: alsiaLabels[i],
+        virtualArea: {
+            leftTop,
+            size: { x: 2, y: 1 },
+        },
+    });
+}
+
+function makeSoomLabelBrush(i: number, leftTop: Point): Brush {
+    return fillText({
+        ...commonProps,
+        value: soomLabels[i],
+        virtualArea: {
+            leftTop,
+            size: { x: 1, y: 2 },
+        },
+    });
+}
+
 export default function* makeLabelImage(): Image {
     for (let i = 0; i < 7; i++) {
         const x = 5 + i * 2;
         const y = 1 + i * 2;
 
-        const printAlsia = (leftTop: Point): Brush =>
-            fillText({
-                ...commonProps,
-                value: alsiaLabels[i],
-                virtualArea: {
-                    leftTop,
-                    size: { x: 2, y: 1 },
-                },
-            });
-        const printSoom = (leftTop: Point): Brush =>
-            fillText({
-                ...commonProps,
-                value: soomLabels[i],
-                virtualArea: {
-                    leftTop,
-                    size: { x: 1, y: 2 },
-                },
-            });
-
-        yield printAlsia({ x, y: 0 });
-        yield printSoom({ x: 4, y });
-        yield printAlsia({ x, y: 15 });
-        yield printSoom({ x: 19, y });
+        yield makeAlsiaLabelBrush(i, { x, y: 0 });
+        yield makeSoomLabelBrush(i, { x: 4, y });
+        yield makeAlsiaLabelBrush(i, { x, y: 15 });
+        yield makeSoomLabelBrush(i, { x: 19, y });
     }
 }
